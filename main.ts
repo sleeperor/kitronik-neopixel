3323d4a70011d994eb0b30b8c095994e1e6983c7
Wojciech Paciorkowski <sleeperor@gmail.com>
2021-12-27 16:32:34 +0100

    lekcja 4

--- main.ts
+++ main.ts
@@ -1,19 +1,13 @@
 input.onButtonPressed(Button.A, function () {
-    Pixel_Array.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
-    Pixel_Array.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
-    Pixel_Array.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
-    Pixel_Array.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
-    Pixel_Array.setPixelColor(4, neopixel.colors(NeoPixelColors.White))
-    Pixel_Array.show()
+    pins.servoWritePin(AnalogPin.P1, 180)
+    pins.servoWritePin(AnalogPin.P2, 180)
+})
+input.onButtonPressed(Button.AB, function () {
+    pins.servoWritePin(AnalogPin.P1, 90)
+    pins.servoWritePin(AnalogPin.P2, 90)
 })
 input.onButtonPressed(Button.B, function () {
-    Pixel_Array.clear()
-    Pixel_Array.show()
+    pins.servoWritePin(AnalogPin.P1, 0)
+    pins.servoWritePin(AnalogPin.P2, 0)
 })
-let Pixel_Array: neopixel.Strip = null
-Pixel_Array = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
-basic.forever(function () {
-    basic.pause(100)
-    Pixel_Array.rotate(1)
-    Pixel_Array.show()
-})
+let Pixel_Array = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
