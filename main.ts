223229127501b34b7108fff3fd04dc269b42002f
Wojciech Paciorkowski <sleeperor@gmail.com>
2021-12-27 15:51:45 +0100

    lekcja 3

--- main.ts
+++ main.ts
@@ -1,7 +1,10 @@
 input.onButtonPressed(Button.A, function () {
-    Pixel_Array.range(0, 2).showColor(neopixel.colors(NeoPixelColors.Red))
-    Pixel_Array.setPixelColor(2, neopixel.colors(NeoPixelColors.Violet))
-    Pixel_Array.range(3, 2).showColor(neopixel.colors(NeoPixelColors.Green))
+    Pixel_Array.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    Pixel_Array.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+    Pixel_Array.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    Pixel_Array.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+    Pixel_Array.setPixelColor(4, neopixel.colors(NeoPixelColors.White))
+    Pixel_Array.show()
 })
 input.onButtonPressed(Button.B, function () {
     Pixel_Array.clear()
@@ -10,5 +13,7 @@ input.onButtonPressed(Button.B, function () {
 let Pixel_Array: neopixel.Strip = null
 Pixel_Array = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
 basic.forever(function () {
-	
+    basic.pause(100)
+    Pixel_Array.rotate(1)
+    Pixel_Array.show()
 })
