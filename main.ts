e76c0c50dce32bee3b4dc7cb0a5ad2e28ac82289
Wojciech Paciorkowski <sleeperor@gmail.com>
2022-01-01 13:41:52 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,20 @@
 input.onButtonPressed(Button.A, function () {
     pins.servoWritePin(AnalogPin.P1, 180)
     pins.servoWritePin(AnalogPin.P2, 180)
+    basic.showIcon(IconNames.Yes)
+    Pixel_Array.showBarGraph(180, 180)
 })
 input.onButtonPressed(Button.AB, function () {
     pins.servoWritePin(AnalogPin.P1, 90)
     pins.servoWritePin(AnalogPin.P2, 90)
+    basic.showIcon(IconNames.Happy)
+    Pixel_Array.showBarGraph(90, 180)
 })
 input.onButtonPressed(Button.B, function () {
     pins.servoWritePin(AnalogPin.P1, 0)
     pins.servoWritePin(AnalogPin.P2, 0)
+    basic.showIcon(IconNames.Yes)
+    Pixel_Array.showBarGraph(0, 180)
 })
-let Pixel_Array = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
+let Pixel_Array: neopixel.Strip = null
+Pixel_Array = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
